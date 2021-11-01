c82b6811b81b3297b342483c95663815ea8911ce
Robert <46486281+Roslon@users.noreply.github.com>
2021-11-01 20:07:12 +0100

    FizzBuzz example

---
+++ FizzBuzz_20211101.hs
@@ -0,0 +1,17 @@
+{-  
+Auth:   Roslon
+Ver.:   2021110111943
+Project: FizzBuzz
+Client: XXX
+-}
+
+-- -----------------------------------------
+fizz :: Int -> String
+fizz n | n `rem` 15 == 0 = "FizzBuzz"
+       | n `rem` 5 == 0  = "Fizz"
+       | n `rem` 3 == 0 = "Buzz"
+       | otherwise = show n
+
+main :: IO ()
+
+main = mapM_ putStrLn $ map fizz [1..20]
