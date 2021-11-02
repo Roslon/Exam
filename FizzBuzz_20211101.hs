418789c519c8d9630a19578bd0491e110598059e
Robert <46486281+Roslon@users.noreply.github.com>
2021-11-02 17:12:37 +0100

    Update FizzBuzz_20211101.hs

--- FizzBuzz_20211101.hs
+++ FizzBuzz_20211101.hs
@@ -1,14 +1,14 @@
 {-  
-Version:   2021110111943
+Version:   202111021712
 Project: FizzBuzz
 -}
-
+-- -----------------------------------------
 fizz :: Int -> String
-fizz n | n `rem` 15 == 0 = "FizzBuzz"
-       | n `rem` 5 == 0  = "Fizz"
-       | n `rem` 3 == 0 = "Buzz"
+fizz n | rem n 15 == 0 = "FizzBuzz"
+       | rem n 5 == 0  = "Fizz"
+       | rem n 3 == 0 = "Buzz"
        | otherwise = show n
 
 main :: IO ()
 
-main = mapM_ putStrLn $ map fizz [1..20]
+main = mapM_ putStrLn $ map fizz [1..20000]
