7eb1c019e07b7217c9766ef31e58852544820c7e
Robert <46486281+Roslon@users.noreply.github.com>
2021-11-17 07:55:06 +0100

    Update number_in_words_pl.hs

--- number_in_words_pl.hs
+++ number_in_words_pl.hs
@@ -1,13 +1,17 @@
 {-  
 author:     Roslon
-version:    202111051535
+version:    202111141922
 project:    Number in words
 -}
 
+import System.Environment
+
 -- -----------------------------------------
-allNumber = 999999
+allNumber :: Int
+allNumber = 123456
 -- -----------------------------------------
 
+ones :: [String]
 ones = [""
         , "jeden "
         , "dwa "
@@ -28,6 +32,8 @@ ones = [""
         , "siedemnaście "
         , "osiemanaście "
         , "dziewiętnaście "]
+
+tens :: [String] 
 tens = [""
         , "dziesięć "
         , "dwadzieścia "
@@ -38,6 +44,8 @@ tens = [""
         , "siedemdziesiąt "
         , "osiemdziesiąt "
         , "dziewięćdziesiąt "]
+
+hundreds :: [String]   
 hundreds = [""
         , "sto "
         , "dwieście "
@@ -49,7 +57,7 @@ hundreds = [""
         , "osiemset "
         , "dziewięćset "]
 
-
+thAdd :: (Eq a, Num a) => a -> String
 thAdd n | n  == 0 = ""
         | n  == 1 = "tysiąc "
         | n  == 2 = "tysiące "
@@ -57,31 +65,59 @@ thAdd n | n  == 0 = ""
         | n  == 4 = "tysiące "
         | otherwise = "tysięcy "
 
-lastNumberO = if (rem allNumber 100) >= 19
+-- jedności i naście
+lastNumberO :: Int
+lastNumberO = if (rem allNumber 100) > 19
         then rem allNumber 10
         else rem allNumber 100
+
+-- część bez jedności
+firstNumberO :: Int
 firstNumberO = div allNumber 10
+
+-- liczba dziesiątek
+lastNumberT :: Int
 lastNumberT = if (rem firstNumberO 10) == 1 
         then 0
         else rem firstNumberO 10
+
+-- liczba bez dziesiątek
+firstNumberT :: Int
 firstNumberT = div firstNumberO 10 
+
+-- liczba setek
+lastNumberH :: Int
 lastNumberH = rem firstNumberT 10
+
+-- liczba bez setek
+firstNumberH :: Int
 firstNumberH = div firstNumberT 10
+
+--liczba pojedynczych tysięcy
+lastNumberTh :: Int
 lastNumberTh = if (rem firstNumberH 100) >=19
         then rem firstNumberH 10
         else rem firstNumberH 100
+
+-- dziesiątki tysięcy
+firstNumberTh :: Int
 firstNumberTh = div firstNumberH 10
+
+-- dzisiątki tysięcy
+lastNumberTTh :: Int
 lastNumberTTh = if (rem firstNumberTh 10) == 1 
      then 0
      else rem firstNumberTh 10
+
+-- setki tysięcy
+firstNumberTTh :: Int  
 firstNumberTTh = div firstNumberTh 10
 
 
 main :: IO ()
-
 main = do 
-     
-    putStrLn $
+  
+       putStrLn $
         "\n"
         ++ hundreds !! firstNumberTTh
         ++ tens !! lastNumberTTh 
@@ -92,5 +128,6 @@ main = do
         ++ hundreds !! lastNumberH 
         ++ tens !! lastNumberT 
         ++ ones !! lastNumberO
-        ++ "\n"
-    
+        ++ "\n"   
+  
+   
